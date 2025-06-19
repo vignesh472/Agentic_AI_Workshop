4b2bd5df99e803b4eda504de0bf29b72e59eab08
vigneshwaran7890 <168219960+vigneshwaran7890@users.noreply.github.com>
2025-06-19 13:25:41 +0530

    updated

--- Day_7-Hackathon copy/src/agents/expectation_retriever.py
+++
@@ -1,9 +0,0 @@
-from rag.rag_index import get_retriever
-from langchain.chains import RetrievalQA
-from utils.common import load_llm
-
-def retrieve_expectations(role, level):
-    retriever = get_retriever()
-    qa_chain = RetrievalQA.from_chain_type(llm=load_llm(), retriever=retriever)
-    query = f"What are the skill expectations for a {role} at {level}?"
-    return qa_chain.run(query)
